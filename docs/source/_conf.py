b41478e1af2a71c33f59047ff41e25ba90766218
G. Arampatzis <gtarabat@gmail.com>
2021-03-25 19:34:13 +0100

    back to original conf.py

--- docs/source/_conf.py
+++ docs/source/_conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'korali'
-copyright = '2021, CSELab'
+copyright = '2020, CSELab'
 author = 'CSELab'
 
 
@@ -28,6 +28,9 @@ author = 'CSELab'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx.ext.todo',
+  'sphinx.ext.mathjax',
+  'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -38,6 +41,14 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -50,3 +61,22 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = True
+
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,54 +1,19 @@
-************************
-Korali's Documentation
-************************
+.. korali documentation master file, created by
+   sphinx-quickstart on Thu Mar 25 19:24:50 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-.. toctree::
-   :caption: Using Korali
-   :maxdepth: 2
-   
-   using/install
-   using/basics 
-   using/parallel
-   using/checkpoints
-   using/tools
-   
-.. toctree::
-   :maxdepth: 2
-   :glob:
-   :caption: Examples
-
-   examples/*
+Welcome to korali's documentation!
+==================================
 
 .. toctree::
    :maxdepth: 2
-   :glob:
-   :caption: Features
+   :caption: Contents:
 
-   features/*
 
-.. toctree::
-   :maxdepth: 2
-   :glob:
-   :caption: Module Configuration
-  
-   modules/experiment/*   
-   modules/problem/*
-   modules/solver/*
-   modules/conduit/*
-   modules/distribution/*
-   modules/neuralNetwork/*
-   
-.. toctree::
-   :maxdepth: 2
-   :caption: Development
 
-   dev/contributing
-   dev/build
-   dev/testing
-   dev/moduledev
-   C++ Class List <https://www.cse-lab.ethz.ch/korali/docs/doxygen/html/annotated.html>
-   Source Files <https://www.cse-lab.ethz.ch/korali/docs/doxygen/html/files.html>
-   Github Repository <https://github.com/cselab/korali>
+Indices and tables
+==================
 
 * :ref:`genindex`
 * :ref:`modindex`
